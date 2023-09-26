56cdc0c53354320dcf11bd927e21a1fb626d5be8
Ing. Joe Florez <joerfrada@gmail.com>
2023-09-26 18:03:52 -0500

    Cambios 22/09/2023 SAML

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -106,7 +106,7 @@ class LoginController extends Controller
                     return response()->json(array('user' => $response, 'tipo' => 0));
                 }
                 else {
-                    return response()->json(array('mensaje' => "El usuario '" . $user->usuario . "' no se encuentra activo.", 'tipo' => -1));
+                    return response()->json(array('mensaje' => "El usuario '" . $user->usuario . "' no se encuentra activo.", 'tipo' => -1, 'codigo' => 2));
                 }
             }
         }
